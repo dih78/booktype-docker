21b8399137e066f03d8d92253a022efd48b5f498
dih78 <dih@ukr.net>
2020-05-25 13:43:14 +0200

    9 commit

--- scripts/celery.sh
+++ scripts/celery.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #/code/venv/bin/python /code/mybook/manage_prod.py celery.py worker --concurrency=10
-python /code/mybook/mybook_site/celery.py worker --concurrency=10
+python /usr/local/bin/celery worker --concurrency=10
