ff070d1c359efa9436ba7da0801915cdeb42b0eb
dih78 <dih@ukr.net>
2020-05-25 09:15:15 +0200

    2 commit

--- scripts/manage_py.sh
+++ scripts/manage_py.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+/code/venv/bin/python /code/mybook/manage_prod.py migrate auth
 /code/venv/bin/python /code/mybook/manage_prod.py migrate
 /code/venv/bin/python /code/mybook/manage_prod.py update_permissions
 /code/venv/bin/python /code/mybook/manage_prod.py update_default_roles
 /code/venv/bin/python /code/mybook/manage_prod.py collectstatic --noinput
 /code/venv/bin/python /code/mybook/manage_prod.py compress
-echo 'from django.contrib.auth.models import User; User.objects.create_superuser("admin", "admin@admin.com", "admin")' | /code/venv/bin/python /code/mybook/manage_dev.py shell
+#echo 'from django.contrib.auth.models import User; User.objects.create_superuser("admin", "admin@admin.com", "admin")' | /code/venv/bin/python /code/mybook/manage_dev.py shell
