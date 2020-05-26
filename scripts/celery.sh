96811434fc5374b40aabd23d84d8d52d60941896
dih78 <dih@ukr.net>
2020-05-26 07:48:36 +0200

    37 commit

--- scripts/celery.sh
+++ scripts/celery.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #/code/venv/bin/python /code/mybook/manage_prod.py celery.py worker --concurrency=10
-/code/venv/bin/python /usr/local/bin/celery -b 'amqp://guest:guest@rabbit:5672/' worker --concurrency=10
+python /usr/local/bin/celery -b 'amqp://guest:guest@rabbit:5672/' worker --concurrency=10
--- scripts/web.sh
+++ scripts/web.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/code/venv/bin/python /code/mybook/manage_prod.py runserver 0.0.0.0:8000
+python /code/mybook/manage_prod.py runserver 0.0.0.0:8000
