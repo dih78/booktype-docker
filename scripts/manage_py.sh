ccafa253fa5f9e5a88d09ec5002be098eb165569
dih78 <dih@ukr.net>
2020-05-26 07:30:19 +0200

    35 commit

--- scripts/manage_py.sh
+++ scripts/manage_py.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#/code/venv/bin/python /code/mybook/manage_prod.py migrate auth
-#/code/venv/bin/python /code/mybook/manage_prod.py migrate
-#/code/venv/bin/python /code/mybook/manage_prod.py update_permissions
-#/code/venv/bin/python /code/mybook/manage_prod.py update_default_roles
-#/code/venv/bin/python /code/mybook/manage_prod.py collectstatic --noinput
-#/code/venv/bin/python /code/mybook/manage_prod.py compress
-python /code/mybook/manage_prod.py migrate auth
-python /code/mybook/manage_prod.py migrate
-python /code/mybook/manage_prod.py update_permissions
-python /code/mybook/manage_prod.py update_default_roles
-python /code/mybook/manage_prod.py collectstatic --noinput
-python /code/mybook/manage_prod.py compress
-echo 'from django.contrib.auth.models import User; User.objects.create_superuser("admin", "admin@admin.com", "admin")' | python /code/mybook/manage_dev.py shell
+/code/venv/bin/python /code/mybook/manage_prod.py migrate auth
+/code/venv/bin/python /code/mybook/manage_prod.py migrate
+/code/venv/bin/python /code/mybook/manage_prod.py update_permissions
+/code/venv/bin/python /code/mybook/manage_prod.py update_default_roles
+/code/venv/bin/python /code/mybook/manage_prod.py collectstatic --noinput
+/code/venv/bin/python /code/mybook/manage_prod.py compress
+#python /code/mybook/manage_prod.py migrate auth
+#python /code/mybook/manage_prod.py migrate
+#python /code/mybook/manage_prod.py update_permissions
+#python /code/mybook/manage_prod.py update_default_roles
+#python /code/mybook/manage_prod.py collectstatic --noinput
+#python /code/mybook/manage_prod.py compress
+echo 'from django.contrib.auth.models import User; User.objects.create_superuser("admin", "admin@admin.com", "admin")' | /code/venv/bin/python /code/mybook/manage_dev.py shell
